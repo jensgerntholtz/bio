44ba806a033628f736cc7a8b949b72912a2514fa
Jens Gerntholtz <jensgerntholtz@Carlins-MacBook-Pro.local>
2018-03-15 14:37:14 +0200

    circular rating scale

---
+++ resources/js/sketch.js
@@ -0,0 +1,51 @@
+var endCounteri = 1;
+var endi = 2*360/5;
+var boxPos  = {};
+var preBoxPos = {};
+var boxIDs = {};
+var boxVals = {};
+
+function setup() {
+  var canvas = createCanvas(100, 100);
+  canvas.position(0,0);
+  angleMode(DEGREES);
+  resizeCanvas(windowWidth, windowHeight);
+  for (var i = 0; i < document.querySelectorAll('.wrapper .box').length; i++) {
+    document.getElementsByClassName('box')[i].id = 'box'+i;
+    document.getElementsByClassName('rating')[i].id = 'rating'+i;
+    boxIDs['boxIDs'+i] = document.getElementById("box"+i);
+    boxVals['boxVal'+i] = (document.getElementById("rating"+i).innerHTML)*360/5;
+    // var endi = boxVal*360/5;
+    if (i!=0) {
+      var j = i-1;
+      preBoxPos['boxPos' + i] = boxIDs['boxIDs'+j].getBoundingClientRect();
+      boxPos['boxPos' + i]= boxIDs['boxIDs'+i].getBoundingClientRect();
+      translate(boxPos['boxPos' + i].left-preBoxPos['boxPos' + i].left,boxPos['boxPos' + i].bottom-preBoxPos['boxPos' + i].bottom);
+    }
+    else {
+      boxPos['boxPos' + i]= boxIDs['boxIDs'+i].getBoundingClientRect();
+      translate(boxPos['boxPos' + i].left,boxPos['boxPos' + i].bottom-200);
+      console.log(boxPos['boxPos' + i].left+','+boxPos['boxPos' + i].bottom-200);
+    }
+    // translate(windowWidth/2, windowHeight/2)
+    // rotate(-90);
+
+    noFill();
+    strokeWeight(15);
+    // arc(100, 100, 160, 160, boxVals['boxVal'+i], 360);
+    stroke(241, 196, 15);
+    // scale out of 5 over 360 degrees
+    var arc1 = arc(100, 100, 160, 160, -90, boxVals['boxVal'+i]-91);
+    window.onscroll = function() {scrollon()};
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+function scrollon() {
+  clear();
+}
+function draw() {
+
+}
