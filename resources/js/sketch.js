516a2c8acc29255710a84c6660e7686e8c445c8c
Jens Gerntholtz <jens@io.co.za>
2018-03-15 16:07:02 +0200

    score tweaks

--- resources/js/sketch.js
+++ resources/js/sketch.js
@@ -14,6 +14,25 @@ function setup() {
     document.getElementsByClassName('rating')[i].id = 'rating'+i;
     boxIDs['boxIDs'+i] = document.getElementById("box"+i);
     boxVals['boxVal'+i] = (document.getElementById("rating"+i).innerHTML)*360/5;
+    if (i!=0) {
+      var j = i-1;
+      preBoxPos['boxPos' + i] = boxIDs['boxIDs'+j].getBoundingClientRect();
+      boxPos['boxPos' + i]= boxIDs['boxIDs'+i].getBoundingClientRect();
+      var boxOffsetX =boxPos['boxPos' + i].left-preBoxPos['boxPos' + i].left;
+      var boxOffsetY =boxPos['boxPos' + i].top-preBoxPos['boxPos' + i].top;
+      translate(boxOffsetX,boxOffsetY);
+    }
+    else {
+      boxPos['boxPos' + i]= boxIDs['boxIDs'+i].getBoundingClientRect();
+      translate(boxPos['boxPos' + i].left,boxPos['boxPos' + i].top);
+      console.log(boxPos['boxPos' + i].left+','+boxPos['boxPos' + i].top);
+    }
+    noFill();
+    strokeWeight(15);
+    // arc(100, 100, 160, 160, boxVals['boxVal'+i], 360);
+    stroke(241, 196, 15);
+    // scale out of 5 over 360 degrees
+    var arc1 = arc(100, 100, 160, 160, -90, boxVals['boxVal'+i]-91);
   }
     // var endi = boxVal*360/5;
 }
@@ -25,28 +44,11 @@ function scrollon() {
   clear();
 }
 function draw() {
-  clear();
+  // clear();
   for (var i = 0; i < document.querySelectorAll('.wrapper .box').length; i++) {
-    if (i!=0) {
-      var j = i-1;
-      preBoxPos['boxPos' + i] = boxIDs['boxIDs'+j].getBoundingClientRect();
-      boxPos['boxPos' + i]= boxIDs['boxIDs'+i].getBoundingClientRect();
-      translate(boxPos['boxPos' + i].left-preBoxPos['boxPos' + i].left,boxPos['boxPos' + i].top-preBoxPos['boxPos' + i].top);
-    }
-    else {
-      boxPos['boxPos' + i]= boxIDs['boxIDs'+i].getBoundingClientRect();
-      translate(boxPos['boxPos' + i].left,boxPos['boxPos' + i].top);
-      console.log(boxPos['boxPos' + i].left+','+boxPos['boxPos' + i].top);
-    }
     // translate(windowWidth/2, windowHeight/2)
     // rotate(-90);
 
-    noFill();
-    strokeWeight(15);
-    // arc(100, 100, 160, 160, boxVals['boxVal'+i], 360);
-    stroke(241, 196, 15);
-    // scale out of 5 over 360 degrees
-    var arc1 = arc(100, 100, 160, 160, -90, boxVals['boxVal'+i]-91);
   }
-  window.onscroll = function() {scrollon()};
+  // window.onscroll = function() {scrollon()};
 }
